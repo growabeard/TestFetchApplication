6b974d2d81e5f1fafd875eabe85acb62aa8ce34e
chris <chris.w.witt@gmail.com>
2025-02-08 14:45:16 -0500

    CW - Adding data model and api service to retrieve list from web.

---
+++ app/src/main/java/com/witt/fetchapplication/network/ListApiService.kt
@@ -0,0 +1,22 @@
+package com.witt.fetchapplication.network
+
+import com.witt.fetchapplication.network.data.Item
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://fetch-hiring.s3.amazonaws.com"
+
+private val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl(
+    BASE_URL).build()
+
+interface ListApiService {
+    @GET("/hiring.json")
+    suspend fun getItemList(): List<Item>
+}
+
+object ListApi {
+    val retrofitService: ListApiService by lazy {
+        retrofit.create(ListApiService::class.java)
+    }
+}
---
+++ app/src/main/java/com/witt/fetchapplication/network/data/Item.kt
@@ -0,0 +1,7 @@
+package com.witt.fetchapplication.network.data
+
+data class Item(
+    var id: Int,
+    var listId: Int,
+    var name: String?
+)
